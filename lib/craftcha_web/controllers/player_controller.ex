010a6a8849fd62474cc4c27e7a63d9bc562a4c6f
Nicolas Sauvage <nicolas.sauvage@talan.com>
2019-08-17 17:50:42 +0200

    cleanup interfaces ; add defaults for players

--- lib/craftcha_web/controllers/player_controller.ex
+++ lib/craftcha_web/controllers/player_controller.ex
@@ -3,19 +3,32 @@ defmodule CraftchaWeb.PlayerController do
   alias Craftcha.Player
   alias Craftcha.Scenario
 
+  @player_defaults %{"hostname" => "", "name" => "", "port" => "3000"}
+
   def new(conn, _params) do
     render conn, "new_player.html"
   end
 
   def create(conn, _params) do
     %{"player" => player} = conn.params
-    %{"hostname" => hostname, "name" => name, "port" => port} = player
+    %{"hostname" => hostname, "name" => name, "port" => port} = Map.merge(@player_defaults, player, &keep_not_empty/3)
+
+    IO.inspect(port, label: "port")
+
     ip = case hostname do
       "" -> :inet_parse.ntoa(conn.remote_ip)
       _ -> hostname
     end
     {:ok, new_player_id} = Player.add_player(ip, name, port)
     redirect(conn, to: player_path(conn, :show, new_player_id))
+  end
+
+  def keep_not_empty(_key, first, second) do
+    case second do
+      nil -> first
+      "" -> first
+      _ -> second
+    end
   end
 
   def index(conn, _params) do
