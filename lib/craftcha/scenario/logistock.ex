9e081073615ed568923faa226178a84e5aedeb68
Nicolas Sauvage <nicolas.sauvage@talan.com>
2019-08-19 10:08:28 +0200

    improve ui

--- lib/craftcha/scenario/logistock.ex
+++ lib/craftcha/scenario/logistock.ex
@@ -40,71 +40,48 @@ defmodule Craftcha.Scenario.Logistock do
   end
 
   def get_tests_level_1 do
-    [test_level_1_0(), test_level_1_1(), test_level_1_2(), test_level_1_4()]
+    request = %HttpRequest{verb: :get, route: "/api/bill", params: [{'test', 'test'}]}
+    checks = [
+      &Validation.check_status(&1, 200),
+      &Validation.check_body(&1, 'Coucou')
+    ]
+    first_test = {request, checks}
+
+    [first_test]
   end
 
-  def test_level_1_0 do
-    request = %HttpRequest{verb: :get, route: '/fizzbuzz', params: [{'value', '3'}]}
-    validations = [&Validation.check_body(&1, 'Fizz'), &Validation.check_status(&1, 200)]
-    {request, validations}
-  end
-
-  def test_level_1_1 do
-    request = %HttpRequest{verb: :get, route: '/fizzbuzz', params: [{'value', '2'}]}
-    validations = [&Validation.check_body(&1, '2'), &Validation.check_status(&1, 200)]
-    {request, validations}
-  end
-
-  def test_level_1_2 do
-    request = %HttpRequest{verb: :get, route: '/fizzbuzz', params: [{'value', '7'}]}
-    validations = [&Validation.check_body(&1, '7')]
-    {request, validations}
-  end
-
-  def test_level_1_4 do
-    request = %HttpRequest{verb: :get, route: '/fizzbuzz', params: [{'value', '12'}]}
-    validations = [&Validation.check_body(&1, 'Fizz'), &Validation.check_status(&1, 200)]
-    {request, validations}
-  end
 
   def get_instructions(level) do
     case level do
       0 -> "
-## Welcome to LogiStock Crafter's Challenge
+# Welcome to LogiStock Crafter's API Challenge
 
-Please start your server on the port you defined when registering.
+Please start your server on the port you defined when registering (default is 3000).
 Your server should:
 - Answer 200 OK with the body *Hello Name* when the route _/_ is called with the method GET, the name to add
 to the message will be passed in the query parameter _name_
 - Answer 404 ERROR when any other route is called
 
 Please click on _Check_ button when you are ready
+
+Every level passed gives 100 points, a wrong anwer on a new level loses 10 points,
+and a wrong answer on a previous level (regression) costs 50 pts.
 "
       1 -> "
-## Fizz
+      Your first task will be to compute the total price of a command
 
-Add an endpoint _/fizzbuzz_
+|Request    |Values                   |
+|-----------|-------------------------|
+|Route      |/api/bill                |
+|Body       |List of items (see below)|
 
-A GET on _/fizzbuzz_ with a query param *value* (integer) should return:
-- *'Fizz'* if the value is a multiple of 3
-- Else: the value in parameter
+|Response   |Values                   |
+|-----------|-------------------------|
+|Route      |/api/bill                |
+|Body       |List of items (see below)|
 "
-      2 -> "
-## Buzz
-
-A GET on _/fizzbuzz_ with a query param *value* (integer) should now return:
-- *'Buzz'* if the value is a multiple of 5
-
-The previous rules still apply
-"
-      3 -> "
-## FizzBuzz
-
-A GET on _/fizzbuzz_ with a query param *value* (integer) should now return:
-- *'FizzBuzz'* if the value is both a multiple of 3 and 5
-
-The previous rules still apply
-"
+      2 -> ""
+      3 -> ""
     end
   end
 
